dd177e1f27d0cec3b5af203f8dc3787bb26fcf06
m-sarabi <m.sarabi.jkd@gmail.com>
2024-08-24 21:14:13 +0330

    Automatically generate patterns dictionary from SQL files in the directory

--- pattern_query/patterns.py
+++ pattern_query/patterns.py
@@ -1,14 +1,10 @@
+import os
+
+SQL_DIR = "pattern_query/sql_files"
 patterns = {
-    'HAMMER': 'pattern_queries/sql_files/hammer.sql',
-
-    'ENGULFING_BULLISH': 'pattern_queries/sql_files/engulfing_bullish.sql',
-    'ENGULFING_BEARISH': 'pattern_queries/sql_files/engulfing_bearish.sql',
-
-    'MORNING_STAR': 'pattern_queries/sql_files/morning_star.sql',
-    'EVENING_STAR': 'pattern_queries/sql_files/evening_star.sql',
-
-    'THREE_WHITE_SOLDIERS': 'pattern_queries/sql_files/three_white_soldiers.sql',
-    'THREE_BLACK_CROWS': 'pattern_queries/sql_files/three_black_crows.sql',
+    os.path.splitext(file)[0].upper(): os.path.join(SQL_DIR, file)
+    for file in os.listdir(SQL_DIR)
+    if file.endswith('.sql')
 }
 
 
