d1e891115dc8b58804240f8307b9b3c98f89a60b
m-sarabi <m.sarabi.jkd@gmail.com>
2024-08-24 13:01:39 +0330

    added `get_pattern` and `list_patterns` functions

--- pattern_queries/patterns.py
+++ pattern_queries/patterns.py
@@ -1,12 +1,20 @@
-HAMMER = 'pattern_queries/hammer.sql'
+patterns = {
+    'HAMMER': 'pattern_queries/hammer.sql',
 
-ENGULFING_BULLISH = 'pattern_queries/engulfing_bullish.sql'
-ENGULFING_BEARISH = 'pattern_queries/engulfing_bearish.sql'
+    'ENGULFING_BULLISH': 'pattern_queries/engulfing_bullish.sql',
+    'ENGULFING_BEARISH': 'pattern_queries/engulfing_bearish.sql',
 
-MORNING_STAR = 'pattern_queries/morning_star.sql'
-EVENING_STAR = 'pattern_queries/evening_star.sql'
+    'MORNING_STAR': 'pattern_queries/morning_star.sql',
+    'EVENING_STAR': 'pattern_queries/evening_star.sql',
 
-THREE_WHITE_SOLDIERS = 'pattern_queries/three_white_soldiers.sql'
-THREE_BLACK_CROWS = 'pattern_queries/three_black_crows.sql'
+    'THREE_WHITE_SOLDIERS': 'pattern_queries/three_white_soldiers.sql',
+    'THREE_BLACK_CROWS': 'pattern_queries/three_black_crows.sql',
+}
 
-ABANDONED_BABY_BULLISH = 'pattern_queries/abandoned_baby_bullish.sql'
+
+def get_pattern(pattern_name):
+    return patterns.get(pattern_name)
+
+
+def list_patterns():
+    return list(patterns.keys())
