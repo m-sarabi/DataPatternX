bfbc548e5cbea022a1f441f0b124a399875bccbd
m-sarabi <m.sarabi.jkd@gmail.com>
2024-08-24 10:18:14 +0330

    Added more pattern constants.

--- pattern_queries/patterns.py
+++ pattern_queries/patterns.py
@@ -1,7 +1,14 @@
-TWO_CROWS = 'two_crows'
-HAMMER = 'hammer'
+DUMMY = 'pattern_queries/dummy.sql'
+HAMMER = 'pattern_queries/hammer.sql'
 
-PATTERN_FILES = {
-    TWO_CROWS: '../pattern_queries/two_crows.sql',
-    HAMMER: '../pattern_queries/hammer.sql'
-}
+# engulfing_bullish, engulfing_bearish
+ENGULFING_BULLISH = 'pattern_queries/engulfing_bullish.sql'
+ENGULFING_BEARISH = 'pattern_queries/engulfing_bearish.sql'
+
+# morning_star, evening_star
+MORNING_STAR = 'pattern_queries/morning_star.sql'
+EVENING_STAR = 'pattern_queries/evening_star.sql'
+
+# three_white_soldiers, three_black_crows
+THREE_WHITE_SOLDIERS = 'pattern_queries/three_white_soldiers.sql'
+THREE_BLACK_CROWS = 'pattern_queries/three_black_crows.sql'
