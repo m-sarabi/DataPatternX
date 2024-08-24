b56a6a2d56d442ffc423b8d0cbe66713190c0189
m-sarabi <m.sarabi.jkd@gmail.com>
2024-08-24 20:55:54 +0330

    fixed the case with invalid pattern names

--- db/queries.py
+++ db/queries.py
@@ -7,16 +7,15 @@ class QueryExecutor:
         self.db_connection = db_connection
 
     def execute_pattern_query(self, patterns, table, fetch_all):
-        # query_files = []
-        # if not query_file:
-        #     raise ValueError(f"Pattern '{pattern}' not found")
-
         conn = self.db_connection.get_connection()
         results = []
         try:
             with conn.cursor() as cur:
                 for pattern in patterns:
-                    query_file = get_pattern(pattern)
+                    query_file = get_pattern(pattern.upper())
+                    if not query_file:
+                        print(f'Pattern "{pattern}" not found')
+                        continue
                     with open(query_file, 'r') as file:
                         query = file.read().replace('{{TABLE_NAME}}', table)
 
